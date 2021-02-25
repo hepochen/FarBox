9dfd07965c20433a6701569dcb6e9d96e4246772
Abu Zafar <azmikbal@gmail.com>
2021-02-26 03:35:28 +0600

    Create bd_bn.py
    Translate BN

---
+++ farbox_bucket/i18n/bn/bd_bn.py
@@ -0,0 +1,120 @@
+# coding: utf8
+from __future__ import absolute_import
+
+data = {
+    #  সাধারণভাবে ব্যবহৃত মৌলিক শব্দ
+    'Default':  u''ডিফল্ট'',
+    'Disable':  u'অক্ষম করুন',
+    'Login': u'লগইন',
+    'Preview': u''পূর্বরূপ'',
+    'Apply': u'প্রয়োগ করুন',
+    'SourceCode': u'源码',
+    'Introduction': u'সোর্সকোড',
+    'Alipay': u'আলিপে',
+    'Promo Code': u'প্রচার কোড',
+    'Pay': u'পে',
+    'Type': u'প্রকার',
+    'Download': u'ডাউনলোড',
+    'None': u'কিছুই নেই'.
+    'More': u'আরও'',
+    'No, Do Nothing': u''না, কিছুই করবেন না'.
+    'Auto Create': u'অটো তৈরি করুন',
+
+    'Navigation': u''নেভিগেশন'',
+    'SyncLogs': u'সিঙ্কলগস',
+    'Cronjob': u'ক্রোনজব',
+
+    'Invite': u'আমন্ত্রণ করুন',
+    'type': u''টাইপ',
+    'Logout': u'লগআউট'',
+    'Mail Center': u''মেল সেন্টার',
+    'My Sites': u''আমার সাইট',
+    'Task': u'কার্য',
+    'Account': u''অ্যাকাউন্ট'',
+    'Account Info': u''অ্যাকাউন্ট তথ্য'',
+    'License Manager': u''লাইসেন্স পরিচালক',
+    'Log & Usage': u'লগ এবং ব্যবহার',
+
+    'Home': u'হোম',
+    "Contact": u'"যোগাযোগ",
+    'Blog': u'ব্লগ'',
+    'Album': u'অ্যালবাম'',
+    'Archive': u'সংরক্ষণাগার',
+    'Links': u'লিঙ্কস',
+    'Pricing': u'প্রাইসিং',
+    'Price': u'মূল্য',
+    'Document': u'ডকুমেন্ট''',
+    'Doc': u'ডক'',
+    'Link': u'লিংক'
+    'Feed': u'订阅',
+    'Categories': u'分类',
+    'Category': u'分类',
+    'About': u'关于',
+    'Recent': u'সাম্প্রতিক'',
+    'Search': u''অনুসন্ধান',
+    'Previous': u'পরবর্তী',
+    'Next': u'পূর্ববর্তী',
+    'Yes': u''হ্যাঁ'',
+    'No': u'না',
+
+
+    'Resume': u'简历',
+    'Tool':  u'工具',
+    'Biz':  u'商业',
+
+    # admin
+    'Setup': u'设置',
+    'Help': u'帮助',
+    'Theme': u'主题',
+    'Theme & App': u'主题 & App',
+    'File': u'文件',
+    'FileManager': u'文件管理器',
+    'Statistic': u'统计',
+    'Delete': u'删除',
+
+    # dashboard
+    'Nav': u'导航',
+    'Title': u'标题',
+    'Sub Title': u'副标题',
+    'Visitor Password': u'访客密码',
+    'Domains': u'多域名',
+    'Domain': u'域名',
+    'Subdomain': u'二级域名',
+    'Save': u'保存',
+    'Keyword': u'关键词',
+    'Description': u'描述',
+    'Site': u'网站',
+    'Render': u'呈现',
+    'Advanced': u'高级',
+    'Footer': u'页底',
+    'Inject Template': u'嵌入模板',
+    'Save Configs': u'保存配置',
+    'TimeZone': u'时区',
+
+    'Hide Post Prefix URL': u'隐藏日志前缀URL',
+    'Show TOC': u'显示 TOC',
+    'Force SSL': u'强制 SSL',
+
+    'Hide Comments': u'隐藏评论',
+    'Cache Strategy': u'缓存策略',
+    'Fast': u'快速',
+    'Balance': u'平衡',
+    'Post Content Type':  u'日志内容类型',
+    'Posts Per Page': u'每页日志数',
+    'Images Per Page': u'每页图片数',
+    'Post Paragraph Indent': u'日志首行缩进',
+    'Flowchart': u'流程图',
+
+    'Max Image Width': u'最大图片宽度',
+    'Max Image Height': u'最大图片高度',
+    'Image Resize Fixed': u''চিত্র পুনরায় আকার ফিক্সড'.
+
+    '3rd Comments Script': u'তৃতীয় মন্তব্যগুলির স্ক্রিপ্ট'',
+
+    #  সচারাচর ব্যবহৃত
+    'by date and desc': u'আপনার তারিখ এবং ডেস্ক',
+    'by date and asc': u'তারিখ অনুসারে ক্রমবর্ধমান ক্রম',
+
+    "please custom your template or choose a theme for site first, no homepage found.": u"দয়া করে আপনার টেম্পলেটটি কাস্টমাইজ করুন বা সাইটের জন্য কোনও থিম চয়ন করুন, কোনও হোমপেজ পাওয়া যায় না। "
+
+}
